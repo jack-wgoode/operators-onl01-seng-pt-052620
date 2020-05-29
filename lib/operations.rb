211ff4c3a08191984dd55034b98fe7da2211d7c0
Jack Goode <jack.w.goode@icloud.com>
2020-05-29 00:08:18 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+   if !(40...60).include?(speed) 
+     true
+   else
+     false
+   end
 end
 
 
-
 def not_safe?(speed)
-	
+	 (speed <40 || speed > 60)? true: false
 end
 	
 
